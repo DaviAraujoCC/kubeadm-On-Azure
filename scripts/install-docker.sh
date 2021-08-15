dde38c9f0130196dc9815b38b57d501924d9cd54
Davi Araújo Caetano <70732391+DaviAraujoCC@users.noreply.github.com>
2021-08-15 17:01:18 -0300

    comments change

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#Instalação do docker
+#Docker installation
 
 curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
 
-#Correção erro versão alpha
+#Alpha version kubeadm correction
 sudo sed -i 's/sock$/sock --exec-opt native.cgroupdriver=systemd/g' /lib/systemd/system/docker.service
 
 sudo systemctl daemon-reload
